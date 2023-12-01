56df22ef07fa4e90e13ed9a5913c4ea1e1daf391
moo-light <moo-light>
2023-12-01 12:28:10 +0700

    update

--- Quizlet Random.js
+++ Quizlet Random.js
@@ -3,12 +3,13 @@ ff = () => {
     console.log("calling ff")
 
   b = random(a);
-
+  b.querySelector('.SetPageTerm-smallSide').style.width = '60%';
+  b.querySelector('.SetPageTerm-largeSide').style.width = '40%';
   wordText = b.querySelector(".SetPageTerm-wordText");
 
   wordText.style.opacity = 1;
   definitionText = b.querySelector(".SetPageTerm-definitionText");
-  definitionText.style.opacity = 0;
+  // definitionText.style.opacity = 0;
   setTimeout(show, 8000);
   function show() {
     definitionText.style.opacity = 1;
@@ -16,12 +17,12 @@ ff = () => {
   function random(items) {
     return items[Math.floor(Math.random() * items.length)];
   }
-  definitionText.scrollIntoView({ block: "center", inline: "nearest" });
+  b.scrollIntoView({ block: "center", inline: "center" });
 };
 var NextB;
 NextB ??= document.createElement("button");
 document.body.append(NextB);
-
+NextB.className = "AssemblyButtonBase AssemblySecondaryButton AssemblySecondaryButton--filled AssemblyButtonBase--large"
 NextB.innerText = "Next";
 NextB.type = "button";
 NextB.style.padding = "2em 3em";
